856648764a4aadd94cea59d73cd2b686db70f0fd
Cesar Bailon <michelxdstyle@gmail.com>
2025-09-29 19:07:42 -0600

    updated content due to user's specification

--- src/StickyCards/stickyCardsData.js
+++ src/StickyCards/stickyCardsData.js
@@ -4,36 +4,36 @@ const stickyCardsData = [
     title: "Hospedaje - Cabaña Chica",
     image: "/images/chicas/chica.jpg",
     description:
-      "Nuestras cabañas están diseñadas exclusivamente para su descanso. <br />La Cabaña Chica ofrece una experiencia acogedora con luz solar, agua caliente 24h, refrigerador de gas y acceso a más de 4,000 m² de jardín con juegos infantiles y cancha multiusos. <br />Tarifa Rack: $3,500.",
+      "Nuestras cabañas están diseñadas exclusivamente para su descanso. <br />La Cabaña Chica ofrece una experiencia acogedora con luz solar, agua caliente 24h, refrigerador de gas y acceso a más de 4,000 m² de jardín con juegos infantiles y cancha multiusos.",
   },
   {
     index: "",
     title: "Hospedaje - Cabaña Grande",
     image: "/images/grandes/grande.jpg",
     description:
-      "Disfrute de un espacio más amplio en nuestra Cabaña Grande, ideal para el descanso. <br />Equipada con luz solar, agua caliente 24h, refrigerador de gas y acceso a nuestras extensas áreas verdes. <br />Tarifa Rack: $4,500.",
+      "Disfrute de un espacio más amplio en nuestra Cabaña Grande, ideal para el descanso. <br />Equipada con luz solar, agua caliente 24h, refrigerador de gas y acceso a nuestras extensas áreas verdes.",
   },
   {
     index: "",
     title: "Servicios de la Cabaña",
-    image: "/images/chicas/cocina.jpg",
+    image: "/images/agapandos-aereo-2.jpg",
     description:
       "Todas nuestras cabañas cuentan con luz proporcionada por paneles solares (no apta para aparatos de alto consumo como secadoras o cafeteras de resistencia).<br />Disfrute de agua caliente las 24 horas (boiler de gas) y un refrigerador de gas con congelador para mantener sus alimentos frescos. <br />Además, tendrá acceso exclusivo a nuestra área verde de más de 4,000 m² con juegos infantiles y cancha multiusos.",
   },
-  {
-    index: "",
-    title: "Cortesías Incluidas",
-    image: "/images/chicas/estancia.jpg",
-    description:
-      "Para su comodidad, incluimos 1 rollo de papel sanitario y 1 jabón de mano por baño por estancia, 1 garrafa de agua (5lt) y 1 jabón biodegradable para trastes por estancia. <br />Se proporciona una carga de leña por día con tarifa regular, o una carga por estancia en caso de promoción (no se venden cargas extras).",
-  },
-  {
-    index: "",
-    title: "Convenios y Experiencias",
-    image: "/images/agapandos-aereo.jpg",
-    description:
-      "Estamos trabajando en establecer convenios con parques y prestadores de servicios locales para ofrecerle experiencias gastronómicas, tours y eventos. <br />La información detallada se compartirá tan pronto como los convenios estén habilitados.",
-  },
+  // {
+  //   index: "",
+  //   title: "Cortesías Incluidas",
+  //   image: "/images/chicas/estancia.jpg",
+  //   description:
+  //     "Para su comodidad, incluimos 1 rollo de papel sanitario y 1 jabón de mano por baño por estancia, 1 garrafa de agua (5lt) y 1 jabón biodegradable para trastes por estancia. <br />Se proporciona una carga de leña por día con tarifa regular, o una carga por estancia en caso de promoción (no se venden cargas extras).",
+  // },
+  // {
+  //   index: "",
+  //   title: "Convenios y Experiencias",
+  //   image: "/images/agapandos-aereo.jpg",
+  //   description:
+  //     "Estamos trabajando en establecer convenios con parques y prestadores de servicios locales para ofrecerle experiencias gastronómicas, tours y eventos. <br />La información detallada se compartirá tan pronto como los convenios estén habilitados.",
+  // },
 ];
 
 export default stickyCardsData;
